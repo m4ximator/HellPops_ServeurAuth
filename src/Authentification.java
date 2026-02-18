f3120c381c3572138de540cbb2b282249bfa37e4
m4ximator <maxime.cherraf@gmail.com>
2026-02-18 15:42:26 +0100

    segmentation

---
+++ src/Authentification.java
@@ -0,0 +1,31 @@
+package serveur.auth;
+
+import commun.interfaces.IAuthService;
+
+public class Authentification implements IAuthService {
+
+	@Override
+	public void inscription() {
+		
+		
+	}
+
+	@Override
+	public void connexion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deconnexion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delivrerJeton() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
