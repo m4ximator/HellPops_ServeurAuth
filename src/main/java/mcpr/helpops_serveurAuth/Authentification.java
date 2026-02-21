41d61bba27dbed4ba20e8e41f33d7dcf822e144a
m4ximator <maxime.cherraf@univ-tlse3.fr>
2026-02-21 22:09:16 +0100

    Première version serveur auth fonctionnel

--- src/main/java/mcpr/helpops_serveurAuth/Authentification.java
+++ src/main/java/mcpr/helpops_serveurAuth/Authentification.java
@@ -1,31 +1,77 @@
 package mcpr.helpops_serveurAuth;
 
-import commun.interfaces.IAuthService;
+import mcpr.hellpops_interfaces.IAuthService;
+import mcpr.hellpops_interfaces.Jeton;
 
-public class Authentification implements IAuthService {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-	@Override
-	public void inscription() {
-		
-		
-	}
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-	@Override
-	public void connexion() {
-		// TODO Auto-generated method stub
+public class Authentification extends UnicastRemoteObject implements IAuthService {
 
-	}
+    private final Map<String, User> sessionsActives = new ConcurrentHashMap<>();
+    private final List<User> utilisateursEnBase = new CopyOnWriteArrayList<>();
 
-	@Override
-	public void deconnexion() {
-		// TODO Auto-generated method stub
+    public Authentification() throws RemoteException {
+        super();
+    }
 
-	}
+    @Override
+    public void inscription(String username, String passwd) {
+        User user = new User(username, passwd);
+        utilisateursEnBase.add(user);
+        //ecriture dans le fichier json
+    }
 
-	@Override
-	public void delivrerJeton() {
-		// TODO Auto-generated method stub
+    @Override
+    public Jeton connexion(String username, String password) throws RemoteException {
+        StringBuilder chaine = new StringBuilder();
+        User userTrouve = chercherUser(username, password);
 
-	}
+        if (userTrouve != null) {
+            Jeton jeton = delivrerJeton();
+            sessionsActives.put(jeton.getValeur(), userTrouve);
+            chaine.append("Connexion reussie pour : ").append(username);
+            System.out.println(chaine.toString());
+            return jeton;
+        }
+        chaine.append("Tentative de connexion echouee pour : ").append(username);
+        System.out.println(chaine.toString());
+        return null;
+    }
+
+    @Override
+    public void deconnexion() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private Jeton delivrerJeton() {
+        long deuxJours = 2L * 24 * 60 * 60 * 1000;
+        Date dateExp = new Date(System.currentTimeMillis() + deuxJours);
+        return new Jeton(dateExp);
+    }
+
+    @Override
+    public boolean estValide(Jeton jeton) throws RemoteException {
+        // Vérification validite jeton
+        if (jeton == null) return false;
+        return sessionsActives.containsKey(jeton.getValeur()) && jeton.getDateExpiration().after(new Date());
+    }
+
+    public User chercherUser(String username, String password) {
+        //ajouter du hashage et chiffrement
+        for (User user : utilisateursEnBase) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null;
+    }
 
 }
--- src/main/java/mcpr/helpops_serveurAuth/Main.java
+++ src/main/java/mcpr/helpops_serveurAuth/Main.java
@@ -1,17 +1,23 @@
 package mcpr.helpops_serveurAuth;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String args[]) {
+        try {
+            // 1. Démarre le rmi registry
+            LocateRegistry.createRegistry(1099);
+
+            // 2. Instancie objet
+            Authentification obj = new Authentification();
+
+            // 3. Déclare l'objet auprès du serveur de noms
+            Naming.rebind("AuthService", obj);
+
+            System.out.println("Le Serveur d'Authentification est déclaré et prêt !");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
