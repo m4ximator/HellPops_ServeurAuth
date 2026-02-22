868b418338a5b4dcbe6fe42d0f8d648015852b78
m4ximator <maxime.cherraf@univ-tlse3.fr>
2026-02-22 22:22:02 +0100

    user

---
+++ src/main/java/mcpr/helpops_serveurAuth/User.java
@@ -0,0 +1,24 @@
+package mcpr.helpops_serveurAuth;
+
+public class User {
+
+    private final String username;
+    private final String password;
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
+
