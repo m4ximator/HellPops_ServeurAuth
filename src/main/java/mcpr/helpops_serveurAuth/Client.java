4e0a90031b34b3b3adb902bfaa82c735215fa328
ADN1345 <adnane.bensaid@univ-tlse3.fr>
2026-03-07 00:46:35 +0100

    suppression classe filles, update de la classe mère + création enum role

--- src/main/java/mcpr/helpops_serveurAuth/Client.java
+++
@@ -1,9 +0,0 @@
-package mcpr.helpops_serveurAuth;
-
-public class Client extends User {
-
-    public Client (String username, String password) {
-        super(username, password, UTILISATEUR);
-
-    }
-}
