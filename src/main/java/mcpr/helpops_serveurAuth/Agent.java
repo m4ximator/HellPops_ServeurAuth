f6ee00264590d02a6a7830576cf05afe118a392e
ADN1345 <adnane.bensaid@univ-tlse3.fr>
2026-03-07 00:49:29 +0100

    Delete src/main/java/mcpr/helpops_serveurAuth/Agent.java

--- src/main/java/mcpr/helpops_serveurAuth/Agent.java
+++
@@ -1,9 +0,0 @@
-package mcpr.helpops_serveurAuth;
-
-public class Agent  extends User {
-
-    public Agent (String username, String password) {
-        super(username, password, AGENT );
-
-    }
-}
